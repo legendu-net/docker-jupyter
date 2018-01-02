cd6b7cd77e2793a1e464d14dca1475a9710ad04d
Benjamin Du <duchuanlong+git@gmail.com>
2018-01-02 23:56:02 +0800

    launch jupyter notebook instead jupyterlab

--- scripts/03-jupyter.sh
+++ scripts/03-jupyter.sh
@@ -3,10 +3,6 @@
 export HOME=/home/$DOCKER_USER # do NOT remove!!! Necessary for it to work.
 cd $HOME
 
-# codemirror configuration
-codemirror_dir="$HOME/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension"
-mkdir -p "$codemirror_dir"
-cp /settings/commands.json "$codemirror_dir"
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
 
 # .local directory
