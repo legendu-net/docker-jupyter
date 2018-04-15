3037bdc3d249adc1af801e22e7d8dc77dd214073
Benjamin Du <duchuanlong+git@gmail.com>
2018-04-15 13:24:50 +0800

    create $HOME/.jupyter

--- scripts/03-jupyter.sh
+++ scripts/03-jupyter.sh
@@ -3,6 +3,7 @@
 export HOME=/home/$DOCKER_USER # do NOT remove!!! Necessary for it to work.
 cd $HOME
 
+mkdir -p $HOME/.jupyter
 chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.jupyter
 
 # .local directory
