06775390e0521be4c6562e9612b7380510ed1ce7
Ben Chuanlong Du <dclong@users.noreply.github.com>
2019-11-16 03:55:18 -0800

    Update list.py

--- scripts/list.py
+++ scripts/list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-
+import json
 from notebook import notebookapp
 
 servers = list(notebookapp.list_running_servers())
-print(servers)
+print(json.dumps(servers, indent=4))
